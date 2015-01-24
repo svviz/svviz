a08dae7385c67309fe1a0768748b95b0ed3a083d
Noah Spies <nspies@stanford.edu>
2015-01-23 18:39:36 -0800

    should now build on rtfd

--- docs/conf.py
+++ docs/conf.py
@@ -260,8 +260,10 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
+try:
+    import sphinx_rtd_theme
 
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except IOError:
+    pass
