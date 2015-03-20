b2064666576a0231078860808ba74a0e4921f965
Noah Spies <nspies@stanford.edu>
2015-03-20 10:43:27 -0700

    reformatting help text for command-line interface

--- src/svviz/commandline.py
+++ src/svviz/commandline.py
@@ -62,52 +62,69 @@ def parseArgs(args):
         formatter_class=argparse.RawTextHelpFormatter,
         epilog="Breakpoint formats:\n{}\n\nFor an example, run 'svviz demo'.".format(getBreakpointFormatsStr()))
 
-    parser.add_argument("ref", help="reference fasta file (a .faidx index file will be \n"
-        "created if it doesn't exist so you need write permissions for this directory)")
-    parser.add_argument("breakpoints", nargs="*", help="information about the breakpoint to be analyzed; see below for information")
+    parser.add_argument("ref", help=
+        "reference fasta file (a .faidx index file will be created if it doesn't exist so you need \n"
+        "write permissions for this directory)")
+    parser.add_argument("breakpoints", nargs="*", help=
+        "information about the breakpoint to be analyzed; see below for information")
 
     requiredParams = parser.add_argument_group("required parameters")
-    requiredParams.add_argument("-b", "--bam", action="append", help="sorted, indexed bam file containing reads of interest to plot; \n"
-        "can be specified multiple times to load multiple samples")
+    requiredParams.add_argument("-b", "--bam", action="append", help=
+        "sorted, indexed bam file containing reads of interest to plot; can be specified multiple \n"
+        "times to load multiple samples")
 
     inputParams = parser.add_argument_group("input parameters")
-    inputParams.add_argument("-t", "--type", help="event type: either del[etion], ins[ertion], mei (mobile element insertion), or \n"
-        "batch (for reading variants from a VCF file in batch mode)")
+    inputParams.add_argument("-t", "--type", help=
+        "event type: either del[etion], ins[ertion], mei (mobile element insertion), or batch (for \n"
+        "reading variants from a VCF file in batch mode)")
 
-    inputParams.add_argument("-A", "--annotations", action="append", help="bed file containing annotations to plot; will be compressed and indexed \n"
-        "using samtools tabix in place if needed (can specify multiple annotations files)")
+    inputParams.add_argument("-A", "--annotations", action="append", help=
+        "bed file containing annotations to plot; will be compressed and indexed using samtools \n"
+        "tabix in place if needed (can specify multiple annotations files)")
 
     # Obsolete
     inputParams.add_argument("-o", "--orientation", help=argparse.SUPPRESS)
 
-    inputParams.add_argument("--fasta", help="An additional indexable fasta file specifying insertion sequences \n"
-        "(eg mobile element sequences)")
+    inputParams.add_argument("--fasta", help=
+        "An additional indexable fasta file specifying insertion sequences (eg mobile element \n"
+        "sequences)")
 
-    inputParams.add_argument("-q", "--min-mapq", metavar="MAPQ", default=0, type=float, 
-        help="minimum mapping quality for reads")
+    inputParams.add_argument("-q", "--min-mapq", metavar="MAPQ", default=0, type=float, help=
+        "minimum mapping quality for reads")
     inputParams.add_argument("--pair-min-mapq", metavar="PAIR_MAPQ", default=0,
-        type=float, help="include only read pairs where at least one read end exceeds PAIR_MAPQ")
-    inputParams.add_argument("-a", "--aln-quality", metavar="QUALITY", type=float, 
-        help="minimum score of the Smith-Waterman alignment against the ref or alt allele \nin order to be considered (multiplied by 2)")
-    inputParams.add_argument("--include-supplementary", action="store_true", help="include supplementary alignments "
-        "(ie, those with the 0x800 bit set in the bam flags); \ndefault: false")
+        type=float, help=
+        "include only read pairs where at least one read end exceeds PAIR_MAPQ")
+    inputParams.add_argument("-a", "--aln-quality", metavar="QUALITY", type=float, help=
+        "minimum score of the Smith-Waterman alignment against the ref or alt allele in order to be \n"
+        "considered (multiplied by 2)")
+    inputParams.add_argument("--include-supplementary", action="store_true", help=
+        "include supplementary alignments (ie, those with the 0x800 bit set in the bam flags); \n"
+        "default: false")
 
     interfaceParams = parser.add_argument_group("interface parameters")
-    interfaceParams.add_argument("-v", "--version", action="version", version="svviz version {}".format(svviz.__version__),
-        help="show svviz version number and exit")
-    interfaceParams.add_argument("--no-web", action="store_true", help="don't show the web interface")
-    interfaceParams.add_argument("--save-reads", metavar="OUT_BAM_PATH", help="save relevant reads to this file (bam)")
+    interfaceParams.add_argument("-v", "--version", action="version", version="svviz version {}".format(svviz.__version__), help=
+        "show svviz version number and exit")
+    interfaceParams.add_argument("--no-web", action="store_true", help=
+        "don't show the web interface")
+    interfaceParams.add_argument("--save-reads", metavar="OUT_BAM_PATH", help=
+        "save relevant reads to this file (bam)")
 
-    interfaceParams.add_argument("-e", "--export", metavar="EXPORT", type=str, help="export view to file; exported file format is determined \n"
-        "from the filename extension (automatically sets --no-web)")
-    interfaceParams.add_argument("-O", "--open-exported", action="store_true", help="automatically open the exported file")
-    interfaceParams.add_argument("--thicker-lines", action="store_true", help="Reads are shown with thicker lines, potentially overlapping \n"
-        "one another, but increasing contrast when zoomed out")
+    interfaceParams.add_argument("-e", "--export", metavar="EXPORT", type=str, help=
+        "export view to file; exported file format is determined \n"
+        "from the filename extension unless --format is specified;"
+        "(automatically sets --no-web)")
+    interfaceParams.add_argument("-O", "--open-exported", action="store_true", help=
+        "automatically open the exported file")
+    interfaceParams.add_argument("--thicker-lines", action="store_true", help=
+        "Reads are shown with thicker lines, potentially overlapping one another, but increasing \n"
+        "contrast when zoomed out")
 
-    interfaceParams.add_argument("--summary", metavar="SUMMARY_FILE", help="save summary statistics to this (tab-delimited) file")
+    interfaceParams.add_argument("--summary", metavar="SUMMARY_FILE", help=
+        "save summary statistics to this (tab-delimited) file")
 
     defaults = parser.add_argument_group("presets")
-    defaults.add_argument("--pacbio", action="store_true", help="sets defaults for pacbio libraries")
+    defaults.add_argument("--pacbio", action="store_true", help=
+        "sets defaults for pacbio libraries")
 
     if len(inputArgs)<1:
         parser.print_help()
